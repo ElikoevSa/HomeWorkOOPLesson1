b985579b2daab2d19a2b0cee974d01403ac26a7f
Aleksandr Zhuk <tradesolokarel@mail.ru>
2024-01-25 20:24:50 +0300

    work in progress

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.classes.Animal;
+import org.example.classes.Diseases;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal("Барсик", LocalDate.of(2020,02,12), Diseases.DISEASES_LICHEN);
+
+        System.out.println(Animal.class.getName() + " " + animal);
     }
 }
--- src/main/java/org/example/classes/Animal.java
+++ src/main/java/org/example/classes/Animal.java
@@ -1,4 +1,24 @@
 package org.example.classes;
 
+import java.time.LocalDate;
+
 public class Animal {
+    private String name;
+    private LocalDate date;
+    private Diseases diseases;
+
+    public Animal(String name, LocalDate date, Diseases diseases) {
+        this.name = name;
+        this.date = date;
+        this.diseases = diseases;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", date=" + date +
+                ", diseases=" + diseases +
+                '}';
+    }
 }
